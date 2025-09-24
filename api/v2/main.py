4be517ceeea89bcd67bc587bd7e220fe1427d213
ELmustasje <thomasbarth@live.no>
2025-09-24 17:02:06 +0200

    u

--- api/v2/main.py
+++ api/v2/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
 from asyncio import Lock
+import json
 
 from api.v2.utils.spond import get_next_training_attendees
 
@@ -22,15 +23,35 @@ app.add_middleware(
 lock = Lock()
 
 
+@app.get("/attendeesFromSpond")
+async def get_attendees_from_spond():
+    """Reads the attendees from a file and returns them."""
+    attendees = await get_next_training_attendees()
+    with open("attendees.json", "w") as f:
+        json.dump(attendees, f)
+
+    return JSONResponse(
+        content=get_attendees(),
+        headers={"Cache-Control": "no-store, no-cache, must-revalidate"},
+    )
+
+
 @app.get("/attendees/")
 async def get_attendees():
     """Reads the attendees from a file and returns them."""
-    attendees = await get_next_training_attendees()
+    attendees = {}
+    with open("attendees.json", "r") as f:
+        attendees = json.load(f)
 
     return JSONResponse(
         content=attendees,
         headers={"Cache-Control": "no-store, no-cache, must-revalidate"},
     )
+
+
+# @app.post("/groups/shuffle")
+# async def post_shuffle_attendees(sim_count: int):
+#     return
 
 
 @app.get("/")
